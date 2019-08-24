6dc32e6f4c34e9ce80bfb0c960b644fd58977139
morarupasukaru <moraru.pasukaru@gmail.com>
2019-08-24 11:48:22 +0200

    add angular app

---
+++ angular/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { QuizPageComponent } from './pages/quiz-page/quiz-page.component';
+import { StudyLevelComponent } from './widgets/study-level/study-level.component';
+import { StopwatchComponent } from './widgets/stopwatch/stopwatch.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomePageComponent,
+    QuizPageComponent,
+    StudyLevelComponent,
+    StopwatchComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ angular/src/app/widgets/study-level/study-level.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-study-level',
+  templateUrl: './study-level.component.html',
+  styleUrls: ['./study-level.component.css']
+})
+export class StudyLevelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
